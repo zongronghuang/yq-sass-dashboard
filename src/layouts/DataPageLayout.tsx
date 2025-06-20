e6af50ecbfc641aab5d50541ce69668279da4fb1
Zong-Rong Huang <zongrong.h886@gmail.com>
2025-06-20 16:39:45 +0800

    feat: add clickable sidebar for desktop use

--- src/layouts/DataPageLayout.tsx
+++ src/layouts/DataPageLayout.tsx
@@ -1,4 +1,10 @@
-import { useState, useRef, use } from "react";
+import {
+  useState,
+  useRef,
+  use,
+  type SetStateAction,
+  type Dispatch,
+} from "react";
 import { Outlet, useNavigate, useLocation } from "react-router";
 import clsx from "clsx";
 import {
@@ -14,6 +20,7 @@ import {
   ListItemText,
   ListItemButton,
   Divider,
+  Drawer,
 } from "@mui/material";
 import SwipeableDrawer from "@mui/material/SwipeableDrawer";
 import IconButton from "@mui/material/IconButton";
@@ -25,38 +32,46 @@ import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import GroupIcon from "@mui/icons-material/Group";
 import CameraIndoorIcon from "@mui/icons-material/CameraIndoor";
 import { AuthContext } from "../contexts/AuthContext";
+import { EMAIL_KEY } from "../constants";
 
 export default function DataPageLayout() {
+  const [openDrawer, setOpenDrawer] = useState(false);
+
   return (
     <Box>
-      <NavigationBar />
-      <MiniDrawer />
+      <NavigationBar setOpenDrawer={setOpenDrawer} />
+      <MiniDrawer openDrawer={openDrawer} setOpenDrawer={setOpenDrawer} />
       <Outlet />
     </Box>
   );
 }
 
-function NavigationBar() {
+function NavigationBar({
+  setOpenDrawer,
+}: {
+  setOpenDrawer: Dispatch<SetStateAction<boolean>>;
+}) {
   const { userEmail, logOut } = use(AuthContext);
   const menuAnchorRef = useRef<HTMLLabelElement>(null);
   const [openMenu, setOpenMenu] = useState(false);
+  const localStorageEmail = localStorage.getItem(EMAIL_KEY);
 
   return (
     <AppBar enableColorOnDark>
       <Toolbar className="flex justify-between">
-        <IconButton>
-          <MenuIcon />
+        <IconButton className="sm:!hidden">
+          <MenuIcon onClick={() => setOpenDrawer(true)} />
         </IconButton>
 
         <Button
           component="label"
-          className="!text-white hover:!outline hover:!outline-white"
+          className="!text-white hover:!outline hover:!outline-white !ml-auto"
           ref={menuAnchorRef}
           variant="outlined"
           startIcon={<AccountCircleIcon />}
           onClick={() => setOpenMenu(!openMenu)}
         >
-          {userEmail}
+          {userEmail || localStorageEmail}
         </Button>
         <Menu
           open={openMenu}
@@ -76,77 +91,110 @@ function NavigationBar() {
   );
 }
 
-function MiniDrawer() {
-  const navigate = useNavigate();
-  const { pathname } = useLocation();
-  const [openDrawer, setOpenDrawer] = useState(false);
+function MiniDrawer({
+  openDrawer,
+  setOpenDrawer,
+}: {
+  openDrawer: boolean;
+  setOpenDrawer: Dispatch<SetStateAction<boolean>>;
+}) {
   const iOS =
     typeof navigator !== "undefined" &&
     /iPad|iPhone|iPod/.test(navigator.userAgent);
-  const drawerOptions = [
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+  const options = [
     {
-      name: "Active Users",
+      label: "Active Users",
       icon: <GroupIcon />,
-      path: "active-users",
+      onClick: () => navigate("/data/active-users"),
+      isActive: pathname.includes("active-users"),
     },
     {
-      name: "Daily Store Section Data",
+      label: "Daily Store Sections",
       icon: <CameraIndoorIcon />,
-      path: "daily-store-section",
+      onClick: () => navigate("/data/daily-store-sections"),
+      isActive: pathname.includes("daily-store-sections"),
     },
   ];
 
   return (
-    <SwipeableDrawer
-      anchor="left"
-      disableBackdropTransition={!iOS}
-      disableDiscovery={iOS}
-      open={openDrawer}
-      transitionDuration={1000}
-      onClose={() => setOpenDrawer(false)}
-      onOpen={() => setOpenDrawer(true)}
-      disableSwipeToOpen={false}
-      swipeAreaWidth={30}
-      keepMounted
-    >
-      <div className="flex justify-end items-center h-14">
-        <IconButton
-          onClick={() => {
-            setOpenDrawer(!openDrawer);
-            console.log("change drawer", openDrawer);
-          }}
-        >
-          {openDrawer ? <ChevronLeftIcon /> : <ChevronRightIcon />}
-        </IconButton>
-      </div>
-      <Divider />
-      <List>
-        {drawerOptions.map((option) => (
-          <ListItem
-            key={option.name}
-            disablePadding
-            className={clsx("py-3", {
-              "bg-blue-600 text-white": pathname.includes(option.path),
-            })}
-          >
-            <ListItemButton
-              className="flex gap-2"
-              onClick={() => {
-                navigate(`/data/${option.path}`);
-              }}
+    <>
+      <SwipeableDrawer
+        className="sm:hidden"
+        anchor="left"
+        disableBackdropTransition={!iOS}
+        disableDiscovery={iOS}
+        open={openDrawer}
+        transitionDuration={1000}
+        onClose={() => setOpenDrawer(false)}
+        onOpen={() => setOpenDrawer(true)}
+        disableSwipeToOpen={false}
+        swipeAreaWidth={30}
+        keepMounted
+      >
+        <div className="flex justify-end items-center h-14">
+          <IconButton onClick={() => setOpenDrawer(!openDrawer)}>
+            {openDrawer ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
+        </div>
+        <Divider />
+        <List>
+          {options.map((option) => (
+            <ListItem
+              key={option.label}
+              disablePadding
+              className={clsx("py-3", {
+                "bg-blue-600 text-white": option.isActive,
+              })}
             >
-              <ListItemIcon
-                className={clsx("!min-w-0", {
-                  "!text-white": pathname.includes(option.path),
-                })}
-              >
-                {option.icon}
-              </ListItemIcon>
-              <ListItemText>{option.name}</ListItemText>
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </SwipeableDrawer>
+              <ListItemButton className="flex gap-2" onClick={option.onClick}>
+                <ListItemIcon
+                  className={clsx("!min-w-0", {
+                    "!text-white": option.isActive,
+                  })}
+                >
+                  {option.icon}
+                </ListItemIcon>
+                <ListItemText>{option.label}</ListItemText>
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </SwipeableDrawer>
+
+      <Drawer className="hidden sm:block" variant="permanent" open={openDrawer}>
+        <div className="flex justify-end items-center h-16">
+          <IconButton onClick={() => setOpenDrawer(!openDrawer)}>
+            {openDrawer ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
+        </div>
+        <Divider />
+        <List>
+          {options.map((option) => (
+            <ListItem
+              key={option.label}
+              disablePadding
+              className={clsx("py-3", {
+                "bg-blue-600 text-white": option.isActive,
+              })}
+            >
+              <ListItemButton className="flex gap-2" onClick={option.onClick}>
+                <ListItemIcon
+                  className={clsx("!min-w-0", {
+                    "!text-white": option.isActive,
+                  })}
+                >
+                  {option.icon}
+                </ListItemIcon>
+                <ListItemText className={clsx({ hidden: !openDrawer })}>
+                  {option.label}
+                </ListItemText>
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+    </>
   );
 }
